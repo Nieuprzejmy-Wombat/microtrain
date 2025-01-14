0a6827290ae386b760a7952d2b0bc4abbcc1953f
wombat <u58_makcza_waw@technischools.com>
2025-01-14 23:37:00 +0100

    created central.main(), revised Poll -> Reader

--- central.py
+++ central.py
@@ -1,17 +1,18 @@
 import select
+import config
+from collections import deque
 
-class Poll:
-    def __init__(self, file) -> None:
+
+class Reader:
+    def __init__(self, file: str) -> None:
         self.poll = select.poll()
-        self.file = file
-        self.buffer = []
-
-    def __enter__(self):
+        self.file = open(file, "r", encoding="utf-8")
         self.poll.register(self.file, select.POLLIN | select.POLLPRI)
-        return self
+        self.buffer = deque()
 
-    def __exit__(self, type, value, traceback):
+    def __del__(self):
         self.poll.unregister(self.file)
+        self.file.close()
 
     def __iter__(self):
         return self
@@ -19,16 +20,23 @@ class Poll:
     def __next__(self):
         while not self.buffer:
             self.buffer.extend(self.poll.poll())
-        ev = self.buffer.pop(0)[1]
+        ev = self.buffer.popleft()[1]
         if ev & select.POLLIN or ev & select.POLLPRI:
             return self.file.readline().strip()
+        elif ev & select.POLLHUP:
+            raise StopIteration()
         else:
-            raise StopIteration() 
+            raise IOError(f"unhandled event: {ev}")
 
 
-if __name__ == "__main__":
-    with open("train_report") as f, Poll(f) as poll, open("slow.log", "w") as slow, open("normal.log", "w") as normal, open("fast.log", "w") as fast:
-        for i in poll:
+def main():
+    reader = Reader(config.train_central_pipe_name)
+    with (
+        open("slow.log", "w") as slow,
+        open("normal.log", "w") as normal,
+        open("fast.log", "w") as fast,
+    ):
+        for i in reader:
             msg_type, msg_value = i.split(":")
             match msg_type.strip():
                 case "speed":
@@ -44,3 +52,7 @@ if __name__ == "__main__":
                 case _:
                     print(f"unknown message type: {msg_type}")
                     exit(1)
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 from random import randint, random
 from time import sleep
+import config
 
 stations = ["Adamowo", "Adamowo (leśniczówka)", "Adampol", "Ahlbeck Grenze", "Aleksandrów", "Aleksandrów Kujawski", "Aleksandrów Kujawski Wąskotorowy","Aleksandrów Łódzki","Alfred","Alwernia" ,"Anastazewo" ,"Andaluzja","Andoria","Andrychów","Andrychów Górnica","Andrzejówka","Anielin Gradowo"] 
 stations_len = len(stations)
 
-with open("train_report", "w") as f:
+with open(config.train_central_pipe_name, "w") as f:
     while True:
         for i in range(18):
             print(f"speed: {random() * 180}", file=f, flush=True)
